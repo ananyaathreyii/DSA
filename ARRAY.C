95e3a4a6278772a32139e3d8800f5332108046b0
ananyaathreyii <ananyaathreyi@gmail.com>
2026-01-21 14:59:19 +0530

    Add files via upload

--- ARRAY.C
+++ ARRAY.C
@@ -2,7 +2,7 @@
 #include<conio.h>
 
 int main(){
-int a[3][3],i,j;
+int a[3][3],i,j,b[3][3],s[3][3];
 clrscr();
 printf("enter array elements:");
 for (i=0;i<3;i++){
@@ -17,6 +17,31 @@ for (i=0;i<3;i++){
  }
  printf("\n");
  }
- getch();
- return 0;
+printf("enter second array elements:");
+for(i=0;i<3;i++){
+ for(j=0;j<3;j++){
+ scanf("%d",&b[i][j]);
+}
+}
+printf("second array elements are:\n");
+ for(i=0;i<3;i++){
+  for(j=0;j<3;j++){
+  printf("%d",b[i][j]);
+ }
+ printf("\n");
+}
+printf("adding:\n");
+ for(i=0;i<3;i++){
+  for(j=0;j<3;j++){
+  s[i][j]=a[i][j]+b[i][j];
+}}
+printf("addition of both arrays are:");
+ for(i=0;i<3;i++){
+  for(j=0;j<3;j++){
+  printf("%d",s[i][j]);
+ }
+ printf("\n");
+}
+getch();
+return 0;
 }
